dda5b36b3db43a0d6bd364ac9c1fce482d5bbc7e
Marco Orovecchia <marco@orovecchia.at>
2019-03-10 22:21:39 +0100

    bump to 0.0.2, include pynanoleaf itself

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='pynanoleaf',
-    version='0.0.1',
+    version='0.0.2',
     author='Marco Orovecchia',
     author_email='pynanoleaf@marco.orovecchia.com',
     url='https://github.com/Oro/pynanoleaf',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     description='A Python3 wrapper for the Nanoleaf API',
     install_requires=['requests'],
+    packages=['pynanoleaf'],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
